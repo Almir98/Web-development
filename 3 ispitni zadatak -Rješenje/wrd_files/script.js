3455d9b902692b55fcc668c295f518c046cd736d
Almir98 <almir.tihak@edu.fit.ba>
2020-05-28 00:07:13 +0200

    Not completed javascript

---
+++ 3 ispitni zadatak -Rješenje/wrd_files/script.js
@@ -0,0 +1,31 @@
+$.validator.addMethod(
+    "regex",
+    function(value, element, regexp) {
+        var check = false;
+        return this.optional(element) || regexp.test(value);
+    },
+    "Please check your input."
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
